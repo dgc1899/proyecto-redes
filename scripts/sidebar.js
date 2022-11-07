895512375e77636638d0a898e1b86137cba5b706
dgc1899 <daniel.garcia18@tectijuana.edu.mx>
2022-11-07 13:53:02 -0800

    Add sidebar

---
+++ scripts/sidebar.js
@@ -0,0 +1,21 @@
+var mini = true;
+
+function toggleSidebar() { 
+    if (mini) {
+        console.log("opening sidebar");
+        document.getElementById("pageSidebar").style.width = "250px";
+        document.getElementById("pageContent").style.marginLeft = "300px";
+        document.getElementById("pageHeader").style.marginLeft = "250px";
+        document.getElementById("pageFooter").style.marginLeft = "250px";
+        this.mini = false;
+    }
+    else {
+        console.log("closing sidebar");
+        document.getElementById("pageSidebar").style.width = "60px";
+        document.getElementById("pageContent").style.marginLeft = "100px";
+        document.getElementById("pageHeader").style.marginLeft = "85px";
+        document.getElementById("pageFooter").style.marginLeft = "100px";
+        this.mini = true;
+    }
+    
+}
