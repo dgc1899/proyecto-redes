11c9e7343600c9f5ecd02bfe749864cec0aaf452
danielgarcia0921 <daniel.garcia2@dexcom.com>
2022-12-06 01:21:41 -0800

    Modify CSS

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -7,14 +7,22 @@ function toggleSidebar() {
         document.getElementById("pageContent").style.marginLeft = "300px";
         document.getElementById("pageHeader").style.marginLeft = "250px";
         document.getElementById("pageFooter").style.marginLeft = "250px";
+
+        document.getElementById("pageContent").style.margin = "auto";
+        document.getElementById("pageFooter").style.margin = "auto";
+
         this.mini = false;
     }
     else {
         console.log("closing sidebar");
         document.getElementById("pageSidebar").style.width = "60px";
         document.getElementById("pageContent").style.marginLeft = "100px";
-        document.getElementById("pageHeader").style.marginLeft = "85px";
-        document.getElementById("pageFooter").style.marginLeft = "100px";
+        document.getElementById("pageHeader").style.marginLeft = "100px";
+        document.getElementById("pageFooter").style.marginLeft = "150px";
+
+        document.getElementById("pageContent").style.margin = "auto";
+        document.getElementById("pageFooter").style.margin = "auto";
+
         this.mini = true;
     }
     
